a49ac23ee645852ae83968c59277de4f40174c90
Eric McCormick <edm00se@gmail.com>
2019-07-31 10:40:56 -0500

    updates to AngularJS 1.7.8 (current)
    also formats to cleaner layout, keeps with es5 support (for now)

--- js/emRes.js
+++ js/emRes.js
@@ -1,1 +1,22 @@
-var app=angular.module('myApp',[]).factory('myFactory',['$http',function($http){return $http.get('EricMcCormick.json',{cache:true});}]).controller('myCtrl',function($scope,myFactory){myFactory.success(function(data,status,headers,config){$scope.myName=data.nameBlock;$scope.myJobs=data.work;$scope.myEd=data.education;$scope.myQual=data.qualifications;$scope.myProg=data.progression;$scope.myInt=data.interests;$scope.mySocial=data.social;});});
+var app = angular
+  .module('myApp', [])
+  .factory('myFactory', [
+    '$http',
+    function($http) {
+      return $http.get('EricMcCormick.json', { cache: true });
+    }
+  ])
+  .controller('myCtrl', function($scope, myFactory) {
+    myFactory.then(function(res) {
+      var data = res.data;
+      $scope.myName = data.nameBlock;
+      $scope.myJobs = data.work;
+      $scope.myEd = data.education;
+      $scope.myQual = data.qualifications;
+      $scope.myProg = data.progression;
+      $scope.myInt = data.interests;
+      $scope.mySocial = data.social;
+    }).catch(function(err){
+      console.error(err);
+    });
+  });
